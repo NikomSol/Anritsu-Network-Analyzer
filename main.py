59cc84df3c7a0e58823a5b49b06753e8bb8c5e40
nikom <nikomsol94@gmail.com>
2020-11-13 17:29:17 +0300

    day end

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ print(ser.isOpen())
 
 anr = Anritsu(ser)
 
-anr.set_power(output_pow=-6)
-anr.set_freq(10,1*10**8,log=True)
-anr.set_poins(3)
-anr.set_RBW(13)
-
-anr.set_channel(channel='all')
-anr.set_format(3, channel = 'all')
-anr.set_meas(3,channel = 'ch1')
-anr.set_meas(5,channel = 'ch2')
+# anr.set_power(output_pow=-6)
+# anr.set_freq(10,1*10**8,log=True)
+# anr.set_poins(3)
+# anr.set_RBW(13)
+#
+# anr.set_channel(channel='all')
+# anr.set_format(3, channel = 'all')
+# anr.set_meas(3,channel = 'ch1')
+# anr.set_meas(5,channel = 'ch2')
 
 anr.sweep('single')
 anr.get_data(channel='all',plot=True,save=True)
